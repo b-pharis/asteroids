a1acc9ea803dcbac03f35434154b166a1a09dda3
b_pharis <brandonpharis1991@gmail.com>
2025-02-09 10:18:26 -0500

    added player controls

--- main.py
+++ main.py
@@ -3,20 +3,31 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(x= SCREEN_WIDTH / 2, y= SCREEN_HEIGHT / 2)
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updateable.update(dt)
         screen.fill(color="black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
