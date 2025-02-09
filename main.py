c2d9b00e9839e622f3a176e0b8bd73c0237a0c58
b_pharis <brandonpharis1991@gmail.com>
2025-02-09 12:09:28 -0500

    added asteroids and collision

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame, sys
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    updateable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updateable, drawable)
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     player = Player(x= SCREEN_WIDTH / 2, y= SCREEN_HEIGHT / 2)
+    AsteroidField()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -22,8 +27,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        updateable.update(dt)
+        updatable.update(dt)
         screen.fill(color="black")
+        for ast in asteroids:
+            if ast.collision(player):
+                print("Game Over!")
+                sys.exit()
         for obj in drawable:
             obj.draw(screen)
         pygame.display.flip()
